65b05c069494130f48de716b0de11e2cf7f3fde4
Leo <leonardo.antazu@upeu.edu.pe>
2020-12-13 15:14:30 -0500

    acape

--- Sysrubricas-2020/src/test/java/pe/edu/com/sysrubricas/Sysrubricas2020ApplicationTests.java
+++ Sysrubricas-2020/src/test/java/pe/edu/com/sysrubricas/Sysrubricas2020ApplicationTests.java
@@ -4,11 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import pe.edu.com.sysrubricas.dao.CompetenciaDao;
+
 @SpringBootTest
 class Sysrubricas2020ApplicationTests {
 	@Autowired
+	CompetenciaDao cdao;
 	@Test
 	void contextLoads() {
+		System.out.println(cdao.readxPlan_l(21));
 	}	
 }	
 
