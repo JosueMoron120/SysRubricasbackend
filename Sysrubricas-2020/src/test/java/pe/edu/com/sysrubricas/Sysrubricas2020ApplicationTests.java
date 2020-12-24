a1554a1056ae0dc5dfe5cd6862b9d53b872a52df
Leo <leonardo.antazu@upeu.edu.pe>
2020-12-23 21:26:06 -0500

    Ultimo commit v1

--- Sysrubricas-2020/src/test/java/pe/edu/com/sysrubricas/Sysrubricas2020ApplicationTests.java
+++ Sysrubricas-2020/src/test/java/pe/edu/com/sysrubricas/Sysrubricas2020ApplicationTests.java
@@ -4,15 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import pe.edu.com.sysrubricas.dao.CompetenciaCursoDao;
 import pe.edu.com.sysrubricas.dao.CompetenciaDao;
+import pe.edu.com.sysrubricas.entity.CompetenciaCurso;
 
 @SpringBootTest
 class Sysrubricas2020ApplicationTests {
 	@Autowired
-	CompetenciaDao cdao;
+	private CompetenciaCursoDao c;
 	@Test
 	void contextLoads() {
-		System.out.println(cdao.readxPlan_l(21));
+		System.out.println(c.readDinamic(1));
 	}	
 }	
 
